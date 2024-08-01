0328a61c548e3748aa01603c762b715e8faa7a21
DanielCachro <danielcachro@gmail.com>
2024-08-01 19:25:47 +0200

    ADD: Summary screen

--- 12-practice-project-quiz/src/components/Quiz.jsx
+++ 12-practice-project-quiz/src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import {useState, useCallback} from 'react'
 
 import QUESTIONS from '../questions'
-import quizCompleteImg from '../assets/quiz-complete.png'
 import Question from './Question'
+import Summary from './Summary'
 
 export default function Quiz() {
 	const [userAnswers, setUserAnswers] = useState([])
@@ -17,12 +17,7 @@ export default function Quiz() {
 	const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
 
 	if (quizIsComplete) {
-		return (
-			<div id='summary'>
-				<img src={quizCompleteImg} alt='trophy icon' />
-				<h2>Quiz Completed!</h2>
-			</div>
-		)
+		return <Summary userAnswers={userAnswers} />
 	}
 
 	return (
