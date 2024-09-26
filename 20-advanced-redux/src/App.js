0efd38afde7d0f8e9744b80646cafd9bc02b6662
DanielCachro <danielcachro@gmail.com>
2024-09-26 22:26:58 +0200

    Lessons: 319-322, i.a. More About Action Creator Thunks, Splitting Code, Fixing Bugs, Redux DevTools

--- 20-advanced-redux/src/App.js
+++ 20-advanced-redux/src/App.js
@@ -5,9 +5,7 @@ import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
 import Products from './components/Shop/Products'
 import Notification from './components/UI/Notification'
-import {sendCartData} from './store/cart-slice'
-
-let isInitial = true
+import {sendCartData, fetchCartData} from './store/cart-actions'
 
 function App() {
 	const dispatch = useDispatch()
@@ -16,12 +14,13 @@ function App() {
 	const notification = useSelector(state => state.ui.notification)
 
 	useEffect(() => {
-		if (isInitial) {
-			isInitial = false
-			return
-		}
+		dispatch(fetchCartData())
+	}, [dispatch])
 
-		dispatch(sendCartData(cart))
+	useEffect(() => {
+		if (cart.changed) {
+			dispatch(sendCartData(cart))
+		}
 	}, [cart, dispatch])
 
 	return (
--- 20-advanced-redux/src/store/cart-slice.js
+++ 20-advanced-redux/src/store/cart-slice.js
@@ -1,11 +1,9 @@
 import {createSlice} from '@reduxjs/toolkit'
-import {uiActions} from './ui-slice'
-
-const REALTIME_DATABASE_URL = process.env.REACT_APP_realtimeDatabaseURL
 
 const initialState = {
 	items: [],
 	totalQuantity: 0,
+	changed: false,
 }
 
 const cartSlice = createSlice({
@@ -20,6 +18,7 @@ const cartSlice = createSlice({
 			const newItem = action.payload
 			const existingItem = state.items.find(item => item.id === newItem.id)
 			state.totalQuantity++
+			state.changed = true
 			if (!existingItem) {
 				state.items.push({
 					id: newItem.id,
@@ -37,6 +36,7 @@ const cartSlice = createSlice({
 			const id = action.payload
 			const existingItem = state.items.find(item => item.id === id)
 			state.totalQuantity--
+			state.changed = true
 			if (existingItem.quantity === 1) {
 				state.items = state.items.filter(item => item.id !== id)
 			} else {
@@ -46,51 +46,6 @@ const cartSlice = createSlice({
 		},
 	},
 })
-
-export const sendCartData = cart => {
-	return async dispatch => {
-		dispatch(
-			uiActions.showNotification({
-				status: 'pending',
-				title: 'Sending...',
-				message: 'Sending cart data!',
-			})
-		)
-
-		const sendRequest = async () => {
-			const response = await fetch(`${REALTIME_DATABASE_URL}/cart.json`, {
-				method: 'PUT',
-				body: JSON.stringify(cart),
-			})
-
-			if (!response.ok) {
-				throw new Error('Sending cart data failed.')
-			}
-		}
-
-		try {
-			await sendRequest()
-
-			dispatch(
-				uiActions.showNotification({
-					status: 'success',
-					title: 'Success!',
-					message: 'Sent cart data successfully!',
-				})
-			)
-		} catch (error) {
-			dispatch(
-				uiActions.showNotification({
-					status: 'error',
-					title: 'Error!.',
-					message: 'Sending cart data failed!',
-				})
-			)
-		}
-
-		await sendRequest()
-	}
-}
 
 export const cartActions = cartSlice.actions
 export default cartSlice
