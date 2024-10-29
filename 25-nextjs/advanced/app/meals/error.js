74c4cfcfaf31a4f83e9d550d0d724e63b307b62e
DanielCachro <danielcachro@gmail.com>
2024-10-29 17:48:26 +0100

    Lessons: 430-437, i.a. Outputting Images With Unknown Dimensions; Setting Up a SQLite Database; Fetching Data By Levering NextJS Fullstack Capabilities; Adding a Loading Page; Using Suspense & Streamed Responses; Handling Errors; Handling "Not Found" States; Rendering Data via Dynamic Routes & Route Parameters.

---
+++ 25-nextjs/advanced/app/meals/error.js
@@ -0,0 +1,15 @@
+
+
+'use client'
+
+export default function Error() {
+	return (
+		<main className='error'>
+			<h1>An error occured!</h1>
+			<p>Failed to featch meal data. Please try again later.</p>
+		</main>
+	)
+}
+
+
+
